dfea09b189bcaa302ef18df1d2b9b6e611e7fe25
Diep <diepnh0312002@gmail.com>
2023-10-20 15:09:59 +0700

    Fixed auth and profile

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, UserAdditionalInfo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import DateInput
+
 
 class PostForm(forms.ModelForm):
     body = forms.CharField(
@@ -13,6 +17,17 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['body']
 
+class CreateUserForm(UserCreationForm):
+    class Meta: 
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username' : forms.TextInput(attrs = {'placeholder': 'Nhập tên người dùng...'}),
+            'email'    : forms.TextInput(attrs = {'placeholder': 'Nhập e-mail...'}),
+            'password1': forms.TextInput(attrs = {'placeholder': 'Nhập mật khẩu...'}),
+            'password2': forms.TextInput(attrs = {'placeholder': 'Nhập lại mật khẩu...'})
+        }
+
 class CommentForm(forms.ModelForm):
     comment = forms.CharField(
         label = '',
@@ -24,3 +39,17 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['comment']
+
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta: 
+        model = UserAdditionalInfo
+        fields = ['avt' , 'bg', 'dob', 'gender', 'location', 'description']
+        Choices = [("MALE", "Nam") , ("FEMALE", 'Nữ'), ("Other", 'Không muốn chia sẻ')]
+        
+        widgets = {
+            'dob': DateInput(attrs={'type' : 'date'}), 
+            'gender': forms.Select(choices=Choices),
+            'location': forms.TextInput(attrs = {'placeholder': 'Nhập vị trí...'}),
+            'description': forms.Textarea(attrs = {'placeholder': 'Nhập mô tả...'}),
+        }
